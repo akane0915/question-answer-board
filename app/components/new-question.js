d36c0f0e1a4b51aabfca3467e3baf4a1e69f424e
akane0915 <asialkane@gmail.com>
2017-06-02 11:02:48 -0700

    hide new question form initially and add button to show form

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
   actions: {
+    showQuestionForm: function() {
+      this.set('addNewQuestion', true);
+    },
+
     saveQuestion() {
       var params = {
         content: this.get('content'),
         author: this.get('author'),
         notes: this.get('notes') ? this.get('notes') : "None"
       }
+      this.set('addNewQuestion', false);
       this.sendAction('saveQuestion', params);
     }
   }
